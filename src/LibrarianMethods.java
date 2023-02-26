310df5685558c97336ce41a6e402055f5a85262c
wow <theoblystedt@gmail.com>
2023-02-26 12:20:41 +0100

    first

--- src/LibrarianMethods.java
+++ src/LibrarianMethods.java
@@ -1,11 +1,32 @@
 public class LibrarianMethods {
 
-    String name;
-    int id;
-
-
     public void createUser(String fname, String lName, int id, int sNum, String title, int numBorrowedBooks, int numBookLimit){
 
     }
+
+    //suspend user kan göras i main genom att uppdatera status.
+
+    public void deleteUser(int id){
+
+    }
+
+    public void returnItem(int id, String title){
+        //id och bok title
+        //tar bort mängden utlånat till användare
+        //ökar mängden tillgängliga för boken
+
+    }
+
+    public void checkRole(int id){
+        //kollar id på person
+        //sout title
+    }
+    public void checkAvailablity(String title){
+        //kollar om boken har en mängd > 0
+        //sout "tillgänglig" eller "inte tillgänglig"
+    }
+
+
+
 
 }
--- src/Users.java
+++ src/Users.java
@@ -2,23 +2,81 @@ public class Users {
     int id;
     String fName;
     String lName;
-    String title;
-    int sNum;
+    String title; //bestämmer hur många böcker man kan låna
+    int sNum; //personnummber
     int numOfBorrowedBooks;
-    final int numBooksLimit; //vet inte om detta funkar pga final. Den kanske alltid är null.
+    boolean status; //hur man suspendar användare, true == suspended. Svårt att se hur man implementerar 15 dagar
 
-    Users(int id, String fname, String lName, String title, int sNum, int numOfBorrowedBooks, int numBooksLimit){
+
+
+    Users(int id, String fname, String lName, String title, int sNum, int numOfBorrowedBooks, boolean status){
         this.id = id;
         this.fName = fname;
         this.lName = lName;
         this.title = title;
         this.sNum = sNum;
         this.numOfBorrowedBooks = numOfBorrowedBooks;
-        this.numBooksLimit = numBooksLimit;
+        this.status = status;
+
     }
 
     public void lendItem(String title) {
 
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getsNum() {
+        return sNum;
+    }
+
+    public void setsNum(int sNum) {
+        this.sNum = sNum;
+    }
+
+    public int getNumOfBorrowedBooks() {
+        return numOfBorrowedBooks;
+    }
+
+    public void setNumOfBorrowedBooks(int numOfBorrowedBooks) {
+        this.numOfBorrowedBooks = numOfBorrowedBooks;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
