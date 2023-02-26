8bfff018ef6a888285d04569387df6d519d2825a
wow <theoblystedt@gmail.com>
2023-02-26 11:53:31 +0100

    first

--- src/Books.java
+++ src/Books.java
@@ -1,8 +1,47 @@
 public class Books {
 
     String title;
-    int Isbn;
+    int isbn;
+    int available; // de som inte är utlånade
+    int loaned; //utlånade
 
-    //tror inte det finns något krav att kunna skapa böcker, men behåller än så länge.
 
+    Books(String title, int isbn, int available, int loaned){
+        this.title = title;
+        this.isbn = isbn;
+        this.available = available;
+        this.loaned = loaned;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
+    }
+
+    public int getLoaned() {
+        return loaned;
+    }
+
+    public void setLoaned(int loaned) {
+        this.loaned = loaned;
+    }
 }
--- src/LibrarianMethods.java
+++ src/LibrarianMethods.java
@@ -1,4 +1,4 @@
-public class Librarian {
+public class LibrarianMethods {
 
     String name;
     int id;
@@ -7,6 +7,5 @@ public class Librarian {
     public void createUser(String fname, String lName, int id, int sNum, String title, int numBorrowedBooks, int numBookLimit){
 
     }
-//dsadas
 
 }
