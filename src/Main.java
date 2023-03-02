cba6d45c5ac657d109256a506001457ffd053e8c
wow <theoblystedt@gmail.com>
2023-03-02 17:23:24 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
+
+        //test av databas metod. sout funkar inte eftersom det är av objekt typ, men det är ingen ide att fixa i
+        //databas metoden eftersom den endast ska hämta datan
         DatabaseMethods m = new DatabaseMethods();
 
         List l = m.listOfUsers();
