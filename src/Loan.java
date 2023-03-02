df31d748718728c282ab61dd3d4fe6b883d0f0cb
wow <theoblystedt@gmail.com>
2023-03-02 11:18:06 +0100

    first

---
+++ src/Loan.java
@@ -0,0 +1,47 @@
+import java.util.Date;
+
+public class Loan {
+    int userId;
+    int isbn;
+    int loanId;
+    Date date;
+
+    Loan(int userId, int isbn, Date date, int loanId){
+        this.userId = userId;
+        this.isbn = isbn;
+        this.date = date;
+        this.loanId = loanId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getLoanId() {
+        return loanId;
+    }
+
+    public void setLoanId(int loanId) {
+        this.loanId = loanId;
+    }
+}
---
+++ src/SuspentionStatus.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class SuspentionStatus {
+    int uId;
+    Date startDate;
+    Date endDate;
+    int warnings;
+    boolean isSuspended;
+
+
+}
