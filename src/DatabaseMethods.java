0f4bd186644ebbba19cb42ab7d60401fbbafbf9f
wow <theoblystedt@gmail.com>
2023-03-03 16:01:06 +0100

    first

--- src/DatabaseMethods.java
+++ src/DatabaseMethods.java
@@ -5,7 +5,7 @@ import java.sql.*;
 
 public class DatabaseMethods {
 
-    public static Connection getConnection() throws SQLException, ClassNotFoundException{
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://194.47.178.142:3306/1ik173v23-4";
         String username = "tb222zg";
@@ -13,8 +13,6 @@ public class DatabaseMethods {
         Connection connection = DriverManager.getConnection(url, username, password);
         return connection;
     }
-
-
 
 
     public List<Users> listOfUsers() throws SQLException, ClassNotFoundException {
@@ -26,14 +24,14 @@ public class DatabaseMethods {
         String query = "select * from UserDB";
         ResultSet resultSet = statement.executeQuery(query);
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             int id = resultSet.getInt("id");
             String fName = resultSet.getString("fName");
             String lName = resultSet.getString("lName");
             int titleId = resultSet.getInt("titleId");
             int sNum = resultSet.getInt("sNum");
 
-            Users user = new Users(id,fName,lName,titleId,sNum);
+            Users user = new Users(id, fName, lName, titleId, sNum);
             listOfUsers.add(user);
         }
 
@@ -54,13 +52,13 @@ public class DatabaseMethods {
         String query = "select * from BooksDB";
         ResultSet resultSet = statement.executeQuery(query);
 
-        while (resultSet.next()){
+        while (resultSet.next()) {
             int isbn = resultSet.getInt("isbn");
             String title = resultSet.getString("title");
             int available = resultSet.getInt("available");
             int onLoan = resultSet.getInt("onLoan");
 
-            Books books = new Books(title,isbn,available,onLoan);
+            Books books = new Books(title, isbn, available, onLoan);
             listOfBooks.add(books);
         }
         resultSet.close();
@@ -77,16 +75,16 @@ public class DatabaseMethods {
         Connection connection = getConnection();
         String query = "INSERT INTO UserDB (id,fName,lName,titleId,sNum) values (?,?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(query);
-        preparedStatement.setInt(1,user.getId());
-        preparedStatement.setString(2,user.getfName());
+        preparedStatement.setInt(1, user.getId());
+        preparedStatement.setString(2, user.getfName());
         preparedStatement.setString(3, user.getlName());
-        preparedStatement.setInt(4,user.getTitleId());
-        preparedStatement.setInt(5,user.getsNum());
+        preparedStatement.setInt(4, user.getTitleId());
+        preparedStatement.setInt(5, user.getsNum());
         int rowsInserted = preparedStatement.executeUpdate();
 
         //kanske kan skippa id eftersom att id är autoincrement i databasen
 
-        if(rowsInserted <= 0){
+        if (rowsInserted <= 0) {
             ok = false;
         }
 
@@ -97,19 +95,89 @@ public class DatabaseMethods {
         return ok;
     }
 
-    public boolean deleteUser(int id){
-        boolean ok = true;
+    public List<Loan> listOfLoans() throws SQLException, ClassNotFoundException {
+        List<Loan> listOfLoans = new ArrayList<>();
 
-        //tar bort användare från databasen
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        String query = "select * from Loans";
+        ResultSet resultSet = statement.executeQuery(query);
 
-        return ok;
+        while (resultSet.next()) {
+            int id = resultSet.getInt("userId");
+            Date date = resultSet.getDate("date");
+            int isbn = resultSet.getInt("isbn");
+            int loanId = resultSet.getInt("loan_id");
+
+
+            Loan loan = new Loan(id, isbn, date, loanId);
+            listOfLoans.add(loan);
+        }
+
+        resultSet.close();
+        statement.close();
+        connection.close();
+
+
+        return listOfLoans;
+    }
+
+    public boolean deleteUser(int id) throws SQLException, ClassNotFoundException {
+
+        boolean deleted = false;
+
+
+        Connection connection = getConnection();
+        List<Loan> listOfLoans = listOfLoans();
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement("SELECT * FROM UserDB WHERE id = ?");
+            stmt.setInt(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+
+            if (resultSet.next()) {
+                // kolla om användaren har lån
+                for (Loan loan : listOfLoans) {
+                    if (loan.getUserId() == id) {
+                        System.out.println("User with id: " + id + " has loans and cannot be deleted");
+                        return false;
+                    }
+                }
+
+                stmt = connection.prepareStatement("DELETE FROM UserDB WHERE id = ?");
+                stmt.setInt(1, id);
+                stmt.executeUpdate();
+                deleted = true;
+            } else {
+                System.out.println("User with id: " + id + " does not exist");
+            }
+
+
+            stmt.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            System.out.println("Error deleting user from database: " + e.getMessage());
+            throw e;
+        } finally {
+
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("Error closing connection: " + e.getMessage());
+                throw e;
+            }
+        }
+
+        return deleted;
     }
 
     //public Users userStatus(int id){ //kolla status på användare typ roll, varningar, lån osv.
 
-       // Users x = new Users();
+    // Users x = new Users();
 
-       // return x;
+    // return x;
     //}
 
 
