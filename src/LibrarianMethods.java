9270e0fe00ab0445b5c6629f50f00db5107ebb95
wow <theoblystedt@gmail.com>
2023-03-05 13:41:06 +0100

    first

--- src/LibrarianMethods.java
+++ src/LibrarianMethods.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibrarianMethods {
+
+    DatabaseMethods dm = new DatabaseMethods();
+
+    public void printUserList(){
+        List<Users> usersList = new ArrayList<>();
+
+        for(Users u : usersList){
+            System.out.println("Name: " + u.getfName() + " " + u.getlName() + "(id: " + u.getId()
+            + ", titldId: " + u.getTitleId() + ", Social number: " + u.getsNum() + ")");
+        }
+    }
 
 
 }
